8cb43e84a5c7294fbd3dde0cab84742d8cd15a89
juan <jcjc712@hotmail.com>
2017-06-04 17:50:10 -0500

    first commit

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private $contact;
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+    public function index(Request $request)
+    {
+        return response()->json(
+            $this->contact->all(),200
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3',
+            'last_name' => 'required|min:3',
+            'email' => 'email|required',
+            'comment' => 'required|min:5'
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
+        $this->contact->create($request->all());
+        return response()->json(
+            ["msg"=>'created successfully'],200
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(contact $contact)
+    {
+        //
+    }
+}
